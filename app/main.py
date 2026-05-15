b99358c479b1a0e91dd69d3d0f9bbac001c00a05
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-05-15 21:19:52 +0200

    Fill function "cinema_visit" in "main.py"

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 # write your imports here
-
+from app.cinema import bar, hall
+from app.people import cinema_staff, customer
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    bar.CinemaBar()
+    hall.CinemaHall(hall_number)
+    customer.Customer(customers[0], customers[1])
+    cinema_staff.Cleaner(cleaner)
